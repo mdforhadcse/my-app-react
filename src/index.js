16abd988747228548be672037a0d94c172f75cf1
mdforhadcse <forhad14@cse.pstu.ac.bd>
2020-11-01 11:52:38 +0600

    section 2 complete

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import ReactDOM from 'react-dom';
 
 //create a react component
 const App = () => {
+
+    const buttonText = {text:'Click me!'};
+    const labelText = 'Enter name:';
+
+    // function getButtonText(){
+    //     return 'Click on me'
+    // }
+
     return (
     <div>
-        <label class="label" for="name">Enter name</label>
+        <label class="label" htmlFor="name">{labelText}</label>
         <input id="name" type="text" />
-        <button style={{ backgroundColor:'blue', color: 'white' }}>Submit</button>
+        <button style={{ backgroundColor:'blue', color: 'white' }}>{buttonText.text}</button>
     </div>
     );
 };
